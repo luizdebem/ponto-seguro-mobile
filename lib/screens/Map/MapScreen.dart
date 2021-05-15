84c0583b2fecd4aa6f9f6f3c9d2d9da5bfdaf49d
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2021-05-14 23:37:25 -0300

    Scratch report modal

--- lib/screens/Map/MapScreen.dart
+++ lib/screens/Map/MapScreen.dart
@@ -3,8 +3,40 @@ import 'package:flutter_map/flutter_map.dart';
 import "package:latlong/latlong.dart" as l;
 import 'package:ponto_seguro/components/SideMenu.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   static final routeName = '/map';
+
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  // @TODO @luizdebem componente
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Adicionar ocorrência aqui'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Formulário adicionar ocorrência'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +51,10 @@ class MapScreen extends StatelessWidget {
         options: MapOptions(
           center: l.LatLng(-27.557417, -48.512880),
           zoom: 13.0,
+          onTap: (l.LatLng s) {
+            _showMyDialog();
+            print(s);
+          },
         ),
         layers: [
           TileLayerOptions(
